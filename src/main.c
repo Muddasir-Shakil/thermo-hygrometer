fb5709506329d1ab41fa27961beb7290a2f936db
muddasir shakil <muddasir@hotmail.de>
2025-01-22 19:22:48 +0100

    add mqtt publisher
    Signed-off-by: muddasir shakil <muddasir@hotmail.de>

--- src/main.c
+++ src/main.c
@@ -15,6 +15,9 @@
 #include <lvgl_input_device.h>
 #include "bme280_sensor.h"
 #include "wifi.h"
+#include "mqtt_publisher.h"
+#include <zephyr/net/socket.h>
+#include <zephyr/net/mqtt.h>
 
 #define LOG_LEVEL CONFIG_LOG_DEFAULT_LEVEL
 #include <zephyr/logging/log.h>
@@ -22,10 +25,13 @@ LOG_MODULE_REGISTER(app);
 
 #define WIFI_SSID CONFIG_WIFI_SSID
 #define WIFI_PASSWORD CONFIG_WIFI_PASSWORD
+// /* The mqtt client struct */
+// static APP_BMEM struct mqtt_client client_ctx;
 
 int main(void)
 {   
 	const struct device *display_dev;
+	struct mqtt_client client_ctx;
 	lv_obj_t *bme280_data_label;
 
 	display_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_display));
@@ -48,18 +54,24 @@ int main(void)
 	// start http server, user can configure
 	// check if SSID and password is set
 	wifi_connect(WIFI_SSID, WIFI_PASSWORD);
-	
-
+	start_publisher(&client_ctx);
 	char data_str[100] = {0};
+	char temperature_str[100] = {0};
+	char humidity_str[100] = {0}; 
 	while (1) {
 		struct bme280_data data;
 		if(get_bme280_data(&data) == -1)
 			lv_label_set_text(bme280_data_label, "Data Error");
 		else {
+			sprintf(temperature_str,"%.1f", data.temperature);
+			sprintf(humidity_str,"%.1f", data.humidity);
 			sprintf(data_str, "%.1f C \n%.1f %%", data.temperature, data.humidity);
 			lv_label_set_text(bme280_data_label, data_str);
+			publish(&client_ctx, MQTT_QOS_0_AT_MOST_ONCE, "Temperature", temperature_str);
+			publish(&client_ctx, MQTT_QOS_0_AT_MOST_ONCE, "Humidity", humidity_str);
 		}
 		lv_task_handler();
+		process_mqtt_and_sleep(&client_ctx);
 		k_sleep(K_MSEC(1000));
 	}
 	return 0;
